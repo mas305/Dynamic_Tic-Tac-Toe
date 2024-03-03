a5974cb2c33d9fc9a90bd48564f4178124e442e3
MAS <mostafaamen305@gmail.com>
2024-03-03 06:43:39 +0200

    Key Board Accessibilty

--- script.js
+++ script.js
@@ -129,6 +129,23 @@ const cellClickHandler = (event, index) => {
   //   console.log({ col });
 };
 
+const createCell = (i) => {
+  const cellElementString = `<div class="cell" role="button" tabindex= ${
+    i + 1
+  }><span class="value"></span></div>`;
+  const cellElement = document
+    .createRange()
+    .createContextualFragment(cellElementString);
+
+  cellElement.querySelector(".cell").onclick = (event) =>
+    cellClickHandler(event, i);
+
+  cellElement.querySelector(".cell").onkeydown = (event) => {
+    if (event.key === "Enter") cellClickHandler(event, i);
+  };
+  return cellElement;
+};
+
 const createBoard = () => {
   const container = document.querySelector(".container");
   const board = document.createElement("div");
@@ -136,13 +153,7 @@ const createBoard = () => {
   board.classList.add("board");
 
   for (let i = 0; i < turns; i++) {
-    const cellElementString = `<div class="cell"><span class="value"></span></div>`;
-    const cellElement = document
-      .createRange()
-      .createContextualFragment(cellElementString);
-
-    cellElement.querySelector(".cell").onclick = (event) =>
-      cellClickHandler(event, i);
+    const cellElement = createCell(i);
     board.appendChild(cellElement);
     document.documentElement.style.setProperty("--grid-rows", NUMBER_OF_ROWS);
   }
